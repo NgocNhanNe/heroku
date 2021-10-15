873e50cf729cf91b4f1f4c3c00c49229fa7ef213
nhannngcc19209 <nhannngcc19209@fpt.edu.vn>
2021-10-15 16:27:33 +0700

    Update connection.php

--- connection.php
+++ connection.php
@@ -3,7 +3,6 @@
 // or die("Can not connect database".mysqli_connect_error());
 $conn = pg_connect("postgres://oaqlvsmrdcmjey:e8cbfdd3e649300de52555a46b019f5bd7a3c7fb2039b3f57c4148d78e112707@ec2-107-22-18-26.compute-1.amazonaws.com:5432/d4vd4hj48v9noq");
 
-	echo 'Connected Successfully!!!';
 	if(!$conn)
 	{
 		die("Could not connect to database");
