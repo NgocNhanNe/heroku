9ec0b0404e9aa957e988b149a08c92bf4dfc805b
nhannngcc19209 <nhannngcc19209@fpt.edu.vn>
2021-10-15 14:02:02 +0700

    update connect

--- connection.php
+++ connection.php
@@ -1,4 +1,11 @@
 <?php
-$conn = mysqli_connect('localhost','root','','decora_shopping')
-or die("Can not connect database".mysqli_connect_error());
+// $conn = mysqli_connect('localhost','root','','decora_shopping')
+// or die("Can not connect database".mysqli_connect_error());
+$conn = pg_connect("postgres://oaqlvsmrdcmjey:e8cbfdd3e649300de52555a46b019f5bd7a3c7fb2039b3f57c4148d78e112707@ec2-107-22-18-26.compute-1.amazonaws.com:5432/d4vd4hj48v9noq");
+
+	echo 'Connected Successfully!!!';
+	if(!$conn)
+	{
+		die("Could not connect to database");
+	}
 ?>
