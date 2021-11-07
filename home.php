95205e1520d4f0db969583608d2be3fa370eea6e
nhannngcc19209 <nhannngcc19209@fpt.edu.vn>
2021-11-07 19:50:32 +0700

    up

--- home.php
+++ home.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>DECORAHOUSE.com</title>
+    <title>ATNSTORE.com</title>
     <meta name = " viewport " content = " width = device - width , initial - scale = 1 ">
     <link rel = " stylesheet " href = " http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
     <script src = "https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
