02c33669c201bc632bc50cf1fa6d54bd8f73bd7b
nhannngcc19209 <nhannngcc19209@fpt.edu.vn>
2021-11-07 19:59:32 +0700

    Update home.php

--- home.php
+++ home.php
@@ -136,44 +136,5 @@ include_once("connection.php");
 </div>
 <br>
 <br>
-<div id="myCarousel" class="carousel slide" data-ride="carousel">
-    <!-- Indicators -->
-    <ol class="carousel-indicators">
-      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-      <li data-target="#myCarousel" data-slide-to="1"></li>
-    </ol>
-
-    <!-- Wrapper for slides -->
-  <div class="carousel-inner" role="listbox">
-    <div class="item active">
-        <img src="images/background1.jpg" alt="Image">
-        <div class="carousel-caption"></div>      
-    </div>
-
-    <div class="item">
-        <img src="images/background2.jpg" alt="Image">
-        <div class="carousel-caption"></div>      
-    </div>
-
-    <div class="item">
-        <img src="images/background3.jpg" alt="Image">
-        <div class="carousel-caption"></div>      
-    </div>
-
-    <div class="item">
-        <img src="images/background4.jpg" alt="Image">
-        <div class="carousel-caption"></div>      
-    </div>
-      <!-- Left and right controls -->
-      <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
-        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-        <span class="sr-only"></span>
-      </a>
-      <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
-        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-        <span class="sr-only"></span>
-      </a>
-  </div>
-</div>
 </body>
 </html>
