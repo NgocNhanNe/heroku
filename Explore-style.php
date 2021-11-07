d61835fe8989e7585c182cddd53b37211f27d546
nhannngcc19209 <nhannngcc19209@fpt.edu.vn>
2021-11-07 21:17:25 +0700

    Update Explore-style.php

--- Explore-style.php
+++ Explore-style.php
@@ -1,6 +1,6 @@
 <ul>
-    <li align="center"><div><img src="images/ep1.jpg"  width="1000px" height="500px"></div><h2>Luxury and beautiful interior designs - Worthy of class- Australia style</h2></li>
-    <li align="center"><div><img src="images/ep2.jpg" width="1000px" height="500px"></div><h2>Luxury and beautiful interior designs - Worthy of class- Chinese style</h2></li>
-    <li align="center"><div><img src="images/ep3.jpg"  width="1000px" height="500px"></div><h2>Luxury and beautiful interior designs - Worthy of class- Thailand style</h2></li>
+    <li align="center"><div><img src="images/ep1.jpg"  width="500px" height="500px"></div><h2>Australia store</h2></li>
+    <li align="center"><div><img src="images/ep2.jpg" width="500px" height="500px"></div><h2>Dolls</h2></li>
+    <li align="center"><div><img src="images/ep3.jpg"  width="500px" height="500px"></div><h2>VietNam Store</h2></li>
 </ul>
     
